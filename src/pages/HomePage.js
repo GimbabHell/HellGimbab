3810e9f8895bf867ace2c29a98a758dc073e8fe7
diobo <diobobo@naver.com>
2024-10-06 03:01:41 +0900

    API call from home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 
 const HomePage = () => {
+    const [hello,setHello] = useState("");
 
     const navigate = useNavigate();
 
@@ -10,10 +12,27 @@ const HomePage = () => {
         navigate("/menu");
     }
 
+    const bringAPI =async()=>{
+        
+        return(
+            fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+        ).then(response=>response.json)
+        .then(data=>setHello(data.insult))
+        // const response = await fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json');
+        // const data = await response.json();
+        // setHello(data.insult);
+    }
+
+    useEffect(()=>{
+        bringAPI(); 
+    },[])
+       
+
     return(
         <>
             <p>home</p>
             <div>
+            <h1>{hello}</h1>
             <button onClick={onClickHandler}>매장식사</button>
             <button onClick={onClickHandler}>포장</button>
             </div>
