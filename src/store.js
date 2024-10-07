a7a3484cd5d00da5bf4f5fcbb1b5d1e32a34f634
NJ-KIM11 <alwaysred1116@gmail.com>
2024-10-07 16:35:13 +0900

    100704

--- src/store.js
+++ src/store.js
@@ -1,24 +1,39 @@
 import { create } from "zustand";
 
-// singleOrderStore로 주문 한개에 대한 객체 생성  (페이지 간의 정보 넘겨주는데 활용)
+// orderSingleMenu(메뉴 한개 담기) --> singleOrder(장바구니) --> ordersPerDay(하루에 발생한 주문들) --> stat(모든 주문들)
+// orderSingleMenu로 메뉴 주문 한개에 대한 객체 생성  (페이지 간의 정보 넘겨주는데 활용)
+// 주문담기 시점에 orderSingleMenu 
+
+
 // 결제시점에 singleOrderStore에 있는 전역 상태들을 orderHistory에 하나의 객체로 저장
 // 후 singleOrderStore는 reset
 // orderHistory 는 singleOrder들로 이루어진 배열???
 
 
-export const singleOrderStore = create((set)=>({
+export const orderStore = create((set, get)=>({
 
-    forHere : true,     //true: 매장식사, false: 포장주문
+    takeOut : false,     //false: 매장식사, true: 포장주문
     menuName : '',
     price : '',         // price와 quantity 는 number, string 중에 뭘로 하는지에 따라서 함수에서 state 쓸지 결정됨
     quantity : '',
     details : '',
+    order : [],
 
-    eatPlace : (forHere) => set({forHere}),
+    eatPlace : (takeOut) => set({takeOut}),
 
-    order : (menuName, price, quantity, details) => set({menuName, price, quantity, details}),
+    orderSingleMenu : (menuName, price, quantity, details) => set({menuName, price, quantity, details}),
 
-    reset : () => set({forHere : true, menuName : '', price : '', quantity : '', details : ''})
+    singleOrder : ()=>{
+        const { menuName, price, quantity, details, order } = get();   // 현재 값 접근        
+        if (Array.isArray(order)){
+            const newOrder = [...order, { menuName, price, quantity, details }];
+            set({ order: newOrder});
+        } else{
+            console.error("Order is not an array!!!!!!!!!!!!");
+        }        
+    },
+    
+    reset : () => set({takeOut : false, menuName : '', price : '', quantity : '', details : ''})
 
 }))
 
