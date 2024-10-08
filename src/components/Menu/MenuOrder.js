3916e4e99d4f4cc8e3ef8d89eae07a4c2f30214f
NJ-KIM11 <alwaysred1116@gmail.com>
2024-10-08 12:26:53 +0900

    menu 점검

--- src/components/Menu/MenuOrder.js
+++ src/components/Menu/MenuOrder.js
@@ -1,7 +1,7 @@
 // 장바구니
 // menu 선택 --> detail 선택후 추가 --> menu 창으로 돌아와서 장바구니에 자동추가
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getSingleMenu } from "../../api/MenuApi";
 import { orderStore } from "../../store";
 
@@ -12,6 +12,12 @@ const MenuOrder = ()=>{
     const menuCode2 = 2;
     const menu = getSingleMenu(menuCode);
     const menu2 = getSingleMenu(menuCode2);
+    const menuDetails = {rice : "백미",
+                        vegi : ["지단 빼기", "당근 빼기", "오이 빼기", "적채 빼기"],
+                        dipping : "소스없음",
+                        topping : ["올리브", "청양고추", "갈릭칩"]};
+
+    const [detailPrice, setDetailPrice] = useState(0);
 
     useEffect(()=>{
         orderSingleMenu(menu.name, menu.price, menu.quantity, menu.details);
@@ -19,12 +25,31 @@ const MenuOrder = ()=>{
     },[])
 
 
-    useEffect(()=>{
+    useEffect(()=>{ 
         console.log(order);
     },[order])
 
     const onClickDelete = index =>{
         deleteSingleOrder(index);
+    }
+
+
+    // 가격 계산
+    // 넘어온 details 배열에서 key 값들만 추출
+    // key 값의 배열들 중 밥, 디핑소스, 토핑 있는지 확인
+    // 밥, 디핑소스가 있으면 해당 배열안에 건두부, 소스없음 있는지 확인 respectively
+    // 있으면 가격 계산
+    // 토핑 key 가 있으면...
+    // topping 배열 전체를 for문 돌려서 menuDetail의 subCategoryCode 1005 만으로 이루어진 배열과 비교후 필터??(이중 for문??)
+
+    const detailKeys = Object.keys(menuDetails);
+    const detailValues = Object.values(menuDetails.topping);
+    console.log(`menuDetails : ${menuDetails}`);
+    console.log(`detailKeys : ${detailKeys}`);
+    console.log(`detailValues : ${detailValues}`);
+    console.log(Array.isArray(detailKeys));
+    for(let i =0; i < detailKeys.length; i++){
+
     }
 
 
