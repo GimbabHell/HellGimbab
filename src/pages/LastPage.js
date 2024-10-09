cf2cd05eed4f7547062dc9b7e1649ac56ca984ff
diobo <diobobo@naver.com>
2024-10-09 20:21:08 +0900

    LastPage, Footer

--- src/pages/LastPage.js
+++ src/pages/LastPage.js
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const LastPage = () => {
 
     const [satanUrl, setSatanUrl] = useState("");
-    setSatanUrl("https://loremflickr.com/320/240/satan")
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+
+        const fetchImage =async()=>{
+            const response = await fetch("https://loremflickr.com/320/240/satan");
+            setSatanUrl(response.url);
+        };
+        fetchImage();
+    }, []);
+    
+    const onClickHandler =()=>{
+        navigate("/");
+    };
 
     return(
         <>
         <img src={satanUrl}/>
-        <h3>우리 지옥 김밥 먹어줘서 고맙다!</h3>
+        <h2>결재가 완료되었습니다.</h2>
+        <h3>영수증 하단 주문 번호를 확인해 주세요.</h3>
+        <br/>
+        <h4>"우리 김밥지옥 먹어줘서 고맙다!"</h4>
+        <br/>
+        <button onClick={onClickHandler}>Return to Hell</button>
+        <br/>
+        <Footer/>
+
         </>
     )
 }
