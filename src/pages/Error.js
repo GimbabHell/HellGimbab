3a7d8a9689899c90cdb315cc17ad22a84b882df7
kkandda <diobobo@naver.com>
2024-10-11 15:10:21 +0900

    에러창 만들기

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,15 @@
+import ErorrStyle from "./Error.css";
+
 const Error = () => {
     return(
-        <p>에러!</p>
+        <div className="container">
+            <h1 className="title">ERROR 404</h1>
+            <video className="scared-video" autoPlay loop muted>
+                <source src="../video/iDontKnowIamScared.mp4" type="video/mp4"></source>
+            </video>
+            <p className="scared-text">어..? 이거 뭐야 무서워..</p>
+        </div>
+        
     )
 }
 
